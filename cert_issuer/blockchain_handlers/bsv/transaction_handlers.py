c5c67030fb0a32efb9f2aaae84f9ce07f0d556db
jkr888 <jkr888@gmail.com>
2022-03-05 12:21:44 -0500

    cleanup

--- cert_issuer/blockchain_handlers/bsv/transaction_handlers.py
+++ cert_issuer/blockchain_handlers/bsv/transaction_handlers.py
@@ -42,18 +42,6 @@ class BitcoinTransactionHandler(TransactionHandler):
         # ensure the issuing address has sufficient balance
         balance = self.connector.get_balance(self.issuing_address, self.secret_manager)
 
-        # self.secret_manager.start()
-        # key = bitsv.Key(self.secret_manager.wif, 'test')
-        # self.secret_manager.stop()
-        # balance = int(key.get_balance())
-        # address = key.address
-
-        # if address != self.issuing_address:
-        #     error_message = 'Derived {} address is not the same as issuing {} address'.format(
-        #         address, self.issuing_address)
-        #     logging.error(error_message)
-        #     raise Error(error_message)
-
         transaction_cost = self.transaction_creator.estimate_cost_for_certificate_batch(self.tx_cost_constants)
         logging.info('%s address balance is %d satoshis', self.issuing_address, balance)
         logging.info('Total cost estimate will be %d satoshis', transaction_cost)
@@ -65,14 +53,5 @@ class BitcoinTransactionHandler(TransactionHandler):
             raise InsufficientFundsError(error_message)
 
     def issue_transaction(self, blockchain_bytes):
-
         txid = self.connector.broadcast_op_return(blockchain_bytes, self.secret_manager)
-
-        # self.secret_manager.start()
-        # print(self.secret_manager.wif)
-        # key = bitsv.Key(self.secret_manager.wif, 'test')
-        # list_of_pushdata = ([blockchain_bytes])
-        # txid = key.send_op_return(list_of_pushdata)
-        # self.secret_manager.stop()
-
         return txid
